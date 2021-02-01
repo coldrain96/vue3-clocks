6880e2b73cc7166411a27295a13c0a560300dc72
coldrain96 <Justtryagain961@gmail.com>
2021-02-01 23:04:49 +0300

    add clocks html and styles code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Clocks />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clocks from './components/Clocks.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Clocks
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
 </style>
---
+++ src/components/Clocks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="whole-page vertical-middle">
+    <div class="vertical-middle__child">
+
+      <div class="clock">
+        <ul class="clock__marks"></ul>
+        <div class="clock__hands">
+          <div class="clock__hand clock__hand--hour"></div>
+          <div class="clock__hand clock__hand--minute"></div>
+          <div class="clock__hand clock__hand--second"></div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style src="@/assets/clocks.scss" scoped></style>
